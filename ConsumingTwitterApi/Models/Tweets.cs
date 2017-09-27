12b9ba988d5e547b706b4b492035346cc6990418
dbasargin <dennisbasargin@hotmail.com>
2017-09-27 13:56:52 -0700

    Add project files.

---
+++ ConsumingTwitterApi/Models/Tweets.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumingTwitterApi.Models
+{
+    public class TwitterUser
+    {
+        public string id { get; set; }
+        public string id_str { get; set; }
+        public string name { get; set; }
+        public string screen_name { get; set; }
+        public string location { get; set; }
+        public string url { get; set; }
+        public string description { get; set; }
+        public string verified { get; set; }
+        public string followers_count { get; set; }
+        public string friends_count { get; set; }
+        public string listed_count { get; set; }
+        public string favourites_count { get; set; }
+        public string statuses_count { get; set; }
+        public string created_at { get; set; }
+        public string utc_offset { get; set; }
+        public string time_zone { get; set; }
+        public string geo_enabled { get; set; }
+        public string lang { get; set; }
+        public string profile_image_url_https { get; set; }
+
+
+        public override string ToString()
+        {
+            return name + ", " + screen_name + ", " + location + ", "+ description;
+        }
+    }
+
+    public class Tweet
+    {
+        [JsonProperty("created_at")]
+        public string created_at { get; set; }
+
+        [JsonProperty("id")]
+        public Int64 id { get; set; }
+
+        [JsonProperty("id_str")]
+        public string id_str { get; set; }
+
+        [JsonProperty("text")]
+        public string text { get; set; }
+
+        [JsonProperty("source")]
+        public string source { get; set; }
+
+        [JsonProperty("truncated")]
+        public bool truncated { get; set; }
+
+        [JsonProperty("in_reply_to_status_id")]
+        public string in_reply_to_status_id { get; set; }
+
+        [JsonProperty("in_reply_to_user_id")]
+        public Int64 in_reply_to_user_id { get; set; }
+
+        [JsonProperty("in_reply_to_user_id_str")]
+        public string in_reply_to_user_id_str { get; set; }
+
+        [JsonProperty("in_reply_to_screen_name")]
+        public string in_reply_to_screen_name { get; set; }
+        
+        //public TwitterUser user { get; set; }
+
+        public override string ToString()
+        {
+            return created_at + ", " + id_str + ", " + text;
+        }
+    }
+    
+}
---
+++ ConsumingTwitterApi/Models/TwitterHelperClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsumingTwitterApi.Models
+{
+    public class TwitterHelperClass
+    {
+        public static string getTwitterAccess()
+        {
+            string access_token = "";
+
+            string credentials = "ZGVZSTBKTVBQSThjZmMzdFdPWUVaRm1JMjplb0loV1BRVlhFRUZNNm5HUTBKT2xFakFvNWNqSDd5NmVYU0pVNlU1VklNdzB5aGNTaQ==";
+
+            var post = WebRequest.Create("https://api.twitter.com/oauth2/token") as HttpWebRequest;
+
+            post.Method = "POST";
+
+            post.ContentType = "application/x-www-form-urlencoded";
+
+            post.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+
+            var reqbody = Encoding.UTF8.GetBytes("grant_type=client_credentials");
+            post.ContentLength = reqbody.Length;
+
+            using (var req = post.GetRequestStream())
+            {
+                req.Write(reqbody, 0, reqbody.Length);
+            }
+            try
+            {
+                string respbody = null;
+                using (var resp = post.GetResponse().GetResponseStream())//there request sends
+                {
+                    var respR = new StreamReader(resp);
+                    respbody = respR.ReadToEnd();
+                }
+                //TODO use a library to parse json
+                access_token = respbody.Substring(respbody.IndexOf("access_token\":\"") + "access_token\":\"".Length, respbody.IndexOf("\"}") - (respbody.IndexOf("access_token\":\"") + "access_token\":\"".Length));
+            }
+            catch //if credentials are not valid (403 error)
+            {
+                //TODO
+            }
+            return access_token;
+        }
+
+        public static List<String> getTweet(string access_token) { 
+            var gettimeline = WebRequest.Create("https://api.twitter.com/1.1/statuses/user_timeline.json?count=3&screen_name=DennisBasargin") as HttpWebRequest;
+            gettimeline.Method = "GET";
+            gettimeline.Headers[HttpRequestHeader.Authorization] = "Bearer " + access_token;
+
+            List<String> dataIn = new List<String>();
+
+            try
+            {
+                string respbody = null;
+                using (var resp = gettimeline.GetResponse().GetResponseStream())//there request sends
+                {
+                    var respR = new StreamReader(resp);
+
+                    respbody = respR.ReadToEnd();
+                    
+                }
+
+                //TODO use a library to parse json
+
+                //first Tweet pulling object data Dynamically: THIS IS WORKING
+                dynamic dynTweet = JsonConvert.DeserializeObject(respbody);
+                dataIn.Add(dynTweet[0].text.ToString());
+
+                //To Do: CONVERT TO TWITTER OBJECT
+                //List<Tweet> r = JsonConvert.DeserializeObject<List<Tweet>>(respbody);
+                //foreach(var twit in r)
+                //{
+                //    dataIn.Add(twit.ToString());
+                //}
+            }
+            catch //401 (access token invalid or expired)
+            {
+                    //TODO
+            }
+
+            return dataIn;
+        }
+
+    }
+}

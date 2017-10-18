d1e7104a226e8c6cd568ed6fcc3118ab375f06cf
dbasargin <dennisbasargin@hotmail.com>
2017-10-17 22:35:09 -0700

    Having some fun playing around with Javascript. added a time and date to homepage.

--- ConsumingTwitterApi/App_Start/BundleConfig.cs
+++ ConsumingTwitterApi/App_Start/BundleConfig.cs
@@ -28,7 +28,7 @@ namespace ConsumingTwitterApi
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
--- ConsumingTwitterApi/Scripts/customHomePageScript.js
+++ ConsumingTwitterApi/Scripts/customHomePageScript.js
@@ -13,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
     else {
         btnState.disabled = false;
     }
-
 });
 
 function didUserAddSearchTerm() {
@@ -29,3 +28,41 @@ function didUserAddSearchTerm() {
         btnState.disabled = false;
     }
 };
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var c = document.getElementById('current-time');
+    
+    setInterval(updateTime, 1000);
+
+    function updateTime() {
+        var d = new Date();
+
+        var hours = d.getHours(),
+            minutes = d.getMinutes();
+            ampm = 'AM';
+
+        if (hours > 12) {
+            hours -= 12;
+            ampm = "PM";
+        } else if (hours === 0) {
+            hours = 12;
+        }
+
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+
+        var sep = ':';
+        if (d.getSeconds() % 2 === 1) sep = ' ';
+
+        c.innerHTML = hours + sep + minutes + ' ' + ampm;
+        
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var date = document.getElementById('current-date');
+    var d = new Date();
+    date.innerHTML =d.getMonth() + '/' + d.getDate().toString() + '/' + d.getFullYear();
+});
